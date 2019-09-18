19f439cc4e0f33ba9e3546c55d65853533620bc2
owl-vg-1 <47003077+owl-vg-1@users.noreply.github.com>
2019-09-19 00:01:48 +0300

    start project

---
+++ index.php
@@ -0,0 +1,176 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Pattaya&display=swap" rel="stylesheet">
+	<link rel="shortcut icon" href="favicon.png" type="image/png">
+	<title>ЧудоРучки</title>
+</head>
+<body>
+	<!-- Блок хедера -->
+	<div class="container-fluid" id="head">
+		<div class="row">
+			<!--  Первая часть логотип и название -->
+			<div class="col-md-10" id="log">
+				ЧудоРучки
+			</div>
+			<!--  Вторая часть Вход и регистрация -->
+			<div class="col-md-2 border border-success" id="logreg">
+				<div class="container logreg m-0 p-0 mx-auto">
+					<div class="row login m-0 p-0 btn-block " >
+						<a href="#">Вход</a> 
+					</div>
+					<div class="row registration m-0 p-0 btn-block">
+						<a href="#">Регистрация</a> 
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Блок слайдера -->
+	<div class="container-fluid" id="slider">
+		<div class="row">
+			<div class="col-md-1 border border-success p-0" id="slid">
+				Слайд1
+			</div>
+			<div class="col-md-1 border border-success p-0" id="slid">
+				Слайд2
+			</div>
+			<div class="col-md-1 border border-success p-0" id="slid">
+				Слайд3
+			</div>
+			<div class="col-md-1 border border-success p-0" id="slid">
+				Слайд4
+			</div>
+			<div class="col-md-1 border border-success p-0" id="slid">
+				Слайд5
+			</div>
+			<div class="col-md-1 border border-success p-0" id="slid">
+				Слайд6
+			</div>
+		</div>
+	</div>
+
+	<!-- Центральный блок -->
+	<div class="container-fluid" id="central_div">
+		<div class="row">
+			<!-- Блок левое меню -->
+			<div class="col-md-2 m-0 p-0">
+				<div class="container-fluid" id="left_menu">
+				<div class="row menu m-0 p-0 btn-block " >
+						<a href="#">Конопка меню1</a> 
+					</div>
+					<div class="row menu m-0 p-0 btn-block " >
+						<a href="#">Конопка меню1</a> 
+					</div>
+					<div class="row menu m-0 p-0 btn-block " >
+						<a href="#">Конопка меню2</a> 
+					</div>
+					<div class="row menu m-0 p-0 btn-block " >
+						<a href="#">Конопка меню3</a> 
+					</div>
+					<div class="row menu m-0 p-0 btn-block " >
+						<a href="#">Конопка меню4</a> 
+					</div>
+					<div class="row menu m-0 p-0 btn-block " >
+						<a href="#">Конопка меню5</a> 
+					</div>
+					<div class="row menu m-0 p-0 btn-block " >
+						<a href="#">Конопка меню6</a> 
+					</div>
+					<div class="row menu m-0 p-0 btn-block " >
+						<a href="#">Конопка меню7</a> 
+					</div>
+				</div>
+			</div>
+			<!-- Блок Контент -->
+			<div class="col-md-10">
+	
+	
+	
+			</div>
+		</div>
+	</div>
+	<div class="central_div">
+
+		<!-- Блок Контент -->
+		<div id="contents">
+			<div id="all_news">
+				<div id="news_img">
+					Изображение новости
+				</div>
+				<div id="news">
+					<div id="headline_news">
+						Заголовок новости
+					</div>
+					<div id="text_news">
+						Не следует, однако, забывать о том, что выбранный нами инновационный путь способствует повышению актуальности соответствующих условий активизации. Значимость этих проблем настолько очевидна, что повышение уровня гражданского сознания в значительной степени обуславливает создание новых предложений? Не следует, однако, забывать о том, что курс на социально-ориентированный национальный проект способствует повышению актуальности форм воздействия!
+					</div>
+				</div>
+			</div>
+			<div id="all_news">
+				<div id="news_img">
+					Изображение новости
+				</div>
+				<div id="news">
+					<div id="headline_news">
+						Заголовок новости
+					</div>
+					<div id="text_news">
+						Не следует, однако, забывать о том, что выбранный нами инновационный путь способствует повышению актуальности соответствующих условий активизации. Значимость этих проблем настолько очевидна, что повышение уровня гражданского сознания в значительной степени обуславливает создание новых предложений? Не следует, однако, забывать о том, что курс на социально-ориентированный национальный проект способствует повышению актуальности форм воздействия!
+					</div>
+				</div>
+			</div>
+			<div id="all_news">
+				<div id="news_img">
+					Изображение новости
+				</div>
+				<div id="news">
+					<div id="headline_news">
+						Заголовок новости
+					</div>
+					<div id="text_news">
+						Не следует, однако, забывать о том, что выбранный нами инновационный путь способствует повышению актуальности соответствующих условий активизации. Значимость этих проблем настолько очевидна, что повышение уровня гражданского сознания в значительной степени обуславливает создание новых предложений? Не следует, однако, забывать о том, что курс на социально-ориентированный национальный проект способствует повышению актуальности форм воздействия!
+					</div>
+				</div>
+			</div>
+			<div id="all_news">
+				<div id="news_img">
+					Изображение новости
+				</div>
+				<div id="news">
+					<div id="headline_news">
+						Заголовок новости
+					</div>
+					<div id="text_news">
+						Не следует, однако, забывать о том, что выбранный нами инновационный путь способствует повышению актуальности соответствующих условий активизации. Значимость этих проблем настолько очевидна, что повышение уровня гражданского сознания в значительной степени обуславливает создание новых предложений? Не следует, однако, забывать о том, что курс на социально-ориентированный национальный проект способствует повышению актуальности форм воздействия!
+					</div>
+				</div>
+			</div>
+			<div id="all_news">
+				<div id="news_img">
+					Изображение новости
+				</div>
+				<div id="news">
+					<div id="headline_news">
+						Заголовок новости
+					</div>
+					<div id="text_news">
+						Не следует, однако, забывать о том, что выбранный нами инновационный путь способствует повышению актуальности соответствующих условий активизации. Значимость этих проблем настолько очевидна, что повышение уровня гражданского сознания в значительной степени обуславливает создание новых предложений? Не следует, однако, забывать о том, что курс на социально-ориентированный национальный проект способствует повышению актуальности форм воздействия!
+					</div>
+				</div>
+			</div>
+			<div id="page_list">
+				Список страниц
+			</div>
+		</div>
+	</div>
+	<!-- Блок футер -->
+	<div id="footer">
+		футер
+	</div>
+</body>
+</html>
